4c1433b271e41055bfde61c82b019e9627725020
chaotic-mo <motesraji@gmail.com>
2021-09-06 21:48:59 +0200

    commit 3

---
+++ hello.py
@@ -0,0 +1,13 @@
+a = '''Hello wereld van…                                    
+
+    pppp    y     y   ttttttt   h   h    oooo   nnnn          
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n
+    '''
+
+print (a)
+#naam: Moataz Raji, studentennummer: 99035655
